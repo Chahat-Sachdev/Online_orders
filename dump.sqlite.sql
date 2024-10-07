dd14dd79ce809e242bb4ef99f7256fc4ef7a3fc0
Chahat Sachdev <136610042+Chahat-Sachdev@users.noreply.github.com>
2024-10-07 16:35:07 +0530

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,17 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    product_id INT,
+    quantity INT,
+    price DECIMAL(10, 2)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
